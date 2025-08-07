4088528c50d99b676b454f07caff73f99c067e97
emilio-devesa <92723521+emilio-devesa@users.noreply.github.com>
2025-08-07 09:55:27 +0200

    [pascal/practica_08-09] Added ISO-compliant ClearScreen and WaitForEnter procedures

---
+++ pascal/practica_08-09/src/modules/common/Operations.pas
@@ -0,0 +1,42 @@
+module Operations;
+{   Práctica 2008-2009
+    Command Line program written in Pascal ISO 10206 (Extended Pascal).
+    More info: README.md
+
+    Operations.pas
+    Provides common operations
+}
+
+
+export  Operations = (
+            ClearScreen,
+            WaitForEnter
+);
+
+
+import  StandardInput;
+        StandardOutput;
+
+
+procedure ClearScreen;
+procedure WaitForEnter;
+end;
+
+
+{** Output text user interface operations **}
+
+{ Clears screen using ANSI escape code }
+procedure ClearScreen;
+begin
+    write(chr(27)+'[2J');  (* Clear screen *)
+    write(chr(27)+'[H');   (* Move cursor to top-left *)
+end;
+
+{ Pause and wait for user to press ENTER }
+procedure WaitForEnter;
+begin
+    write('Press ENTER to continue...');
+    readln;
+end;
+
+end.
